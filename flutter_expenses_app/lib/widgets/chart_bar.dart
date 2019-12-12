21e1e2223b4c6e218a8ecbfaa56f8045340489e9
Satinder <grewal@lavabit.com>
2019-12-13 01:40:51 +1300

    Finishing the chart bars

--- flutter_expenses_app/lib/widgets/chart_bar.dart
+++ flutter_expenses_app/lib/widgets/chart_bar.dart
@@ -15,7 +15,9 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        FittedBox(
+          child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        ),
         SizedBox(
           height: 4,
         ),
