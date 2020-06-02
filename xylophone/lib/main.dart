69660aaadd251be1f4edccd2a861b73ad5410cb2
Satinder <grewal@lavabit.com>
2020-06-03 04:33:09 +1200

    xylophone sound test

---
+++ xylophone/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: FlatButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note1.wav');
+              },
+              child: Text('Click Me'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
