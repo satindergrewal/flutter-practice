94f489ea5c212f62fead2a02d2b4b5f02ad2970b
Satinder Grewal <grewal@lavabit.com>
2020-06-06 03:59:26 +1200

    completed clima practice

--- Clima/lib/screens/location_screen.dart
+++ Clima/lib/screens/location_screen.dart
@@ -75,8 +75,8 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {
-                      Navigator.push(
+                    onPressed: () async {
+                      var typedName = await Navigator.push(
                         context,
                         MaterialPageRoute(
                           builder: (context) {
@@ -84,6 +84,10 @@ class _LocationScreenState extends State<LocationScreen> {
                           },
                         ),
                       );
+                      if (typedName != null) {
+                        var weatherData = await weather.getCityWeather(typedName);
+                        updateUI(weatherData);
+                      }
                     },
                     child: Icon(
                       Icons.location_city,
