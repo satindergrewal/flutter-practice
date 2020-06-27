cac1c0a32319bec61abc610ac5703737c326fa43
Satinder Grewal <grewal@lavabit.com>
2020-06-28 04:43:51 +1200

    native_add example cpp example
    https://flutter.dev/docs/development/platform-integration/c-interop

---
+++ native_add/example/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:native_add/native_add.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Text('1 + 2 == ${nativeAdd(1, 2)}'),
+        ),
+      ),
+    );
+  }
+}
