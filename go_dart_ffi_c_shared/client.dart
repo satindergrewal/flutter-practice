8c99f8a9aa0fd61b4cb908919b768a8a1a347df4
Satinder Grewal <grewal@lavabit.com>
2020-07-01 21:32:22 +1200

    greeter_daemon library called from dart

--- go_dart_ffi_c_shared/client.dart
+++ go_dart_ffi_c_shared/client.dart
@@ -1,14 +1,31 @@
 import 'dart:ffi';
 import 'dart:io';
 
+import 'package:ffi/ffi.dart';
+import 'package:args/args.dart';
+
 
 typedef add_func = Int64 Function(Int64, Int64);
 typedef Add = int Function(int, int);
 
+typedef startgrpc_func = void Function();
+typedef StartGrpc = Function();
+
 void main(List<String> args) {
+
+  var parser = ArgParser();
+  var results = parser.parse(args);
+  print(results.rest);
 
   final awesome = DynamicLibrary.open('./awesome.so');
 
   final Add add = awesome.lookup<NativeFunction<add_func>>('Add').asFunction();
   stdout.writeln("awesome.Add(12, 99) = ${add(12, 99)}");
+
+  final greeter = DynamicLibrary.open('./greeter_daemon.so');
+
+  // final StartGrpc startGrpc = greeter.lookup<NativeFunction<void>>('StartGrpc').asFunction();
+
+  final void Function() startGrpc = greeter.lookup<NativeFunction<Void Function()>>("StartGrpc").asFunction();
+  startGrpc();
 }
